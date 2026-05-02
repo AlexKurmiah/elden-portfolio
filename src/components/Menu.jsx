6e904e47c905fe1853eae9419dbd669cd3cd6694
AlexKurmiah <alexkurmiah38@gmail.com>
2026-05-02 15:54:46 -0400

    initial commit

---
+++ src/components/Menu.jsx
@@ -0,0 +1,11 @@
+import "./menu.css";
+
+export default function Menu() {
+  return (
+    <nav className="menu">
+      <button className="menu-btn">About Me</button>
+      <button className="menu-btn">Projects</button>
+      <button className="menu-btn">Contact</button>
+    </nav>
+  );
+}
